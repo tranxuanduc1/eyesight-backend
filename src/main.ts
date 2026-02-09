00f20c789b0c71b92bd4fbdacb6b179389d85739
Duc <tran1106duc@gmail.com>
2026-02-09 09:06:44 -0600

    feat:add document and cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+  origin: [
+    'http://localhost:3000',
+    'https://eyesight.app',
+    'https://admin.eyesight.app',
+  ],
+  credentials: true,
+});
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
