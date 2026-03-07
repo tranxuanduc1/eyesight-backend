669234c762a20a546c06f134d1fb5156dfe551c3
tranxuanduc1 <tran1106duc@gmail.com>
2026-03-07 11:52:34 +0700

    add test

--- src/app.service.ts
+++ src/app.service.ts
@@ -9,18 +9,9 @@ export class AppService {
 
   async getDatabaseStatus(): Promise<string> {
     try {
-      // Example usage with Prisma
-      const user = await prisma.user.create({
-        data: {
-          email: 'user@example.com',
-          name: 'Test User',
-          password: 'securepassword',
-        }
-      });
-      
       const users = await prisma.user.findMany();
       console.log(users);
-      return 'Database connection successful!';
+      return 'Database connection successful !';
     } catch (error) {
       return 'Database connection failed: ' + error.message;
     }
